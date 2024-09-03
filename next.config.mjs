afe79f565ad447f24ad65a46c6df7b2883a98e2a
Rodrigo Luglio <rodrigo@rlabs.com.br>
2024-09-03 11:44:44 -0300

    github pages and actions

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: "export",
+  basePath: "/vscode-theme-generator",
+  images: {
+    unoptimized: true,
+  },
+};
 
 export default nextConfig;
