aff822599738e32381c4747170f7116e36c775b4
Rodrigo Luglio <rodrigo@rlabs.com.br>
2024-10-10 09:20:04 -0300

    test onigasm next config

--- next.config.mjs
+++ next.config.mjs
@@ -8,8 +8,15 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  webpack: function (config, options) {
-    config.options = { ...options, asyncWebAssembly: true }
+  webpack(config, { isServer, dev }) {
+    // Use the client static directory in the server bundle and prod mode
+    // Fixes `Error occurred prerendering page "/"`
+    config.output.webassemblyModuleFilename =
+      isServer && !dev ? '../public/onigasm.wasm' : 'public/onigasm.wasm'
+
+    // Since Webpack 5 doesn't enable WebAssembly by default, we should do it manually
+    config.experiments = { ...config.experiments, asyncWebAssembly: true }
+
     return config
   },
   experimental: {
