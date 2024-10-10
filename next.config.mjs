2324782d0e755da532e0c0723e67171fc3ae462e
Rodrigo Luglio <rodrigo@rlabs.com.br>
2024-10-10 09:30:42 -0300

    test wasm turbo

--- next.config.mjs
+++ next.config.mjs
@@ -8,19 +8,16 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  webpack(config, { isServer, dev }) {
-    // Use the client static directory in the server bundle and prod mode
-    // Fixes `Error occurred prerendering page "/"`
-    config.output.webassemblyModuleFilename =
-      isServer && !dev ? '../public/onigasm.wasm' : 'public/onigasm.wasm'
-
-    // Since Webpack 5 doesn't enable WebAssembly by default, we should do it manually
-    config.experiments = { ...config.experiments, asyncWebAssembly: true }
-
-    return config
-  },
   experimental: {
     turbo: {
+      asyncWebAssembly: true,
+      rules: {
+        webassemblyModuleFilename: [
+          process.env.NODE_ENV === 'production'
+            ? '../public/onigasm.wasm'
+            : 'onigasm.wasm',
+        ],
+      },
       resolveAlias: {
         'monaco-editor': 'monaco-editor/esm/vs/editor/editor.api',
       },
